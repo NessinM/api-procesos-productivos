09af150ec0ebb4090cd4573c286dcc0139d2ceeb
Nesyn Alvarado Bedoya <nalvarado@datacont.com>
2020-02-25 15:44:43 -0500

    socket app

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-var express                          = require('express'),
-    app                              = express(),
-    config                           = require('./util/config'),
-    general                          = require('./util/general'),
-    morgan                           = require('morgan'),
-    path                             = require('path')
-    cors                             = require('cors'),
-    multer                           = require('multer'),
-    cron                             = require('node-cron'),
-    bodyParser                       = require('body-parser');
+var express    = require('express'),
+    app        = express(),
+    config     = require('./util/config'),
+    general    = require('./util/general'),
+    morgan     = require('morgan'),
+    path       = require('path')
+    cors       = require('cors'),
+    multer     = require('multer'),
+    cron       = require('node-cron'),
+    bodyParser = require('body-parser');
+    server     = require('http').Server(app);
+    io         = require('socket.io')(server);
     
-	// parse application/x-www-form-urlencoded
-	app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-	// parse application/json
-	app.use(bodyParser.json());
+// parse application/json
+app.use(bodyParser.json());
 
-	// Add headers
-	app.use(cors());
+// Add headers
+app.use(cors());
 
-	// Statics
-	app.use('/statics', express.static('statics'))
+// Statics
+app.use('/statics', express.static('statics'))
 
 //Para muestra, todos los request a /articulo van al router
 app.use('/usuario', require('./router/usuario'))
 
-app.listen(config.port, function(){
-	console.log('Escuchando en puerto ' + config.port + ' environment ' + config.env)
-});
-
-process.on('uncaughtException', function(err) {
-	if(config.dev) console.error(err.stack);
-	else {
-		general.notifySlack('no se sabe', err, 'no handled error', 'no handled error', function(err){
-		  process.exit(1);
-		});
-	}
+server.listen(config.port, () => {
+	console.log('Servidor corriendo en el puerto ' + config.port + ' ubicado en el entorno de ' + config.env)
 })
+
+io.on('connection', socket => {
+	console.log('Nuevo usuario conectado')
+
+	socket.on('disconnect', () => {
+			console.log('Usuario fue desconectado')
+	})
+})
+
